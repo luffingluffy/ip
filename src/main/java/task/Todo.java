fb90165923e693ebaf78fe9224c3a7714e325a4c
Lutfi Randiva <luffingluffy@gmail.com>
2021-08-24 11:02:30 +0800

    Level-8

--- src/main/java/task/Todo.java
+++ src/main/java/task/Todo.java
@@ -1,9 +1,12 @@
+package task;
+
 public class Todo extends Task {
     private static final String type = "T";
 
     public Todo(String description) {
         super(description);
     }
+
     public Todo(String description, boolean isDone) {
         super(description, isDone);
     }
